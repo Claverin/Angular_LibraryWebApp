21c2ca0e123624e0aa71336e0536e6732c59cde7
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-06-07 12:23:19 +0200

    Add project files.

---
+++ LibraryWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
