141cd47002d49657f655fd5e4484a47d95bfb8ba
Piotr Michalski <mrpiotrmichalski@gmail.com>
2022-06-10 12:35:57 +0200

    minor changes

--- LibraryWebApp_Angular/src/app/rest/endpoints/book-endpoint.service.ts
+++ LibraryWebApp_Angular/src/app/rest/endpoints/book-endpoint.service.ts
@@ -7,25 +7,35 @@ import { BaseEndpointService } from './base-endpoint.service';
 @Injectable({
   providedIn: 'root'
 })
-export class BookEndpointService extends BaseEndpointService{
+export class BookEndpointService extends BaseEndpointService
+{
+    url = "book/"
 
-  constructor(client: HttpClient)
-  {
-    super(client)
-  }
+    constructor(client: HttpClient) {
+        super(client)
+    }
 
-  url = "book"
+    getOne(id: Number): Observable<Book>
+    {
+        return this.get(this.url + id)
+    }
 
-  getOne(id: Number): Observable<Book>
-  {
-    return this.get(this.url + "/" + id)
-  }
+    getAll(): Observable<Book[]>
+    {
+        return this.get(this.url)
+    }
 
-  getAll(): Observable<Book[]> {
-    return this.get(this.url)
-  }
+    create(book: Book): Observable<Book>
+    {
+        return this.post(this.url, book)
+    }
 
-  create(book: Book): Observable<Book> {
-    return this.post(this.url, book)
-  }
+    update(id: Number): Observable<Book> {
+        return this.update(id)
+    }
+
+    delete(id: Number): Observable<Book>
+    {
+        return this.delete(id)
+    }
 }
